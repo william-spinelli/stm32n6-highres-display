4f70209519365e7b6a97feb030dc6a5f5ddfd260
William Spinelli <174336620+william-spinelli@users.noreply.github.com>
2026-05-06 15:54:53 +0200

    Reset framebuffer

--- STM32CubeIDE/Appli/hal_stm_lvgl/tft.c
+++ STM32CubeIDE/Appli/hal_stm_lvgl/tft.c
@@ -4,12 +4,15 @@
 #include "tft.h"
 #include "stm32n6xx.h"
 
+#include <string.h>
+
 #if LV_COLOR_DEPTH != 16 && LV_COLOR_DEPTH != 24 && LV_COLOR_DEPTH != 32
 #error LV_COLOR_DEPTH must be 16, 24, or 32
 #endif
 
+#define FRAMEBUFFER_SIZE (TFT_HOR_RES * TFT_VER_RES * (LV_COLOR_DEPTH >> 3))
 
-uint8_t fb[2][TFT_HOR_RES * TFT_VER_RES * (LV_COLOR_DEPTH >> 3)] __attribute__((section("TouchGFX_Framebuffer"))) __attribute__((aligned(16))) = {};
+uint8_t fb[2][FRAMEBUFFER_SIZE] __attribute__((section("TouchGFX_Framebuffer"))) __attribute__((aligned(16))) = {};
 
 void tft_init(void)
 {
@@ -18,6 +21,8 @@ void tft_init(void)
   /*-----------------------------------
    * Register the display in LVGL
    *----------------------------------*/
+  memset(fb[0], 0, FRAMEBUFFER_SIZE);
+  memset(fb[1], 0, FRAMEBUFFER_SIZE);
   lv_display_t* disp = lv_st_ltdc_create_direct(fb[0], fb[1], 0);
   (void) disp;
 }
