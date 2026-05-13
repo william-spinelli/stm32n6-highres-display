d92063d422c11c2b9b7adc85ea0c345bd0b9b3bb
William Spinelli <174336620+william-spinelli@users.noreply.github.com>
2026-05-13 17:27:27 +0200

    Add touch driver for ILI2132

--- STM32CubeIDE/Appli/hal_stm_lvgl/touchpad.c
+++ STM32CubeIDE/Appli/hal_stm_lvgl/touchpad.c
@@ -10,6 +10,34 @@ extern I2C_HandleTypeDef hi2c2;
 
 #ifdef USE_RIVERDI_DISPLAY
 // ILI2132
+static void touchpad_read(lv_indev_t* indev, lv_indev_data_t* data)
+{
+  uint8_t buf[16];
+  HAL_StatusTypeDef status;
+
+  static int16_t last_x = 0;
+  static int16_t last_y = 0;
+
+  status = HAL_I2C_Mem_Read(&hi2c2, (0x41 << 1), 0x10, 1, buf, sizeof(buf), 100);
+
+  if ((status == HAL_OK) && ((buf[1] & 0x40) != 0) && ((buf[1] & 0x3F) == 0))
+  {
+    int16_t x = (buf[3] & 0x0F) << 8 | buf[2];
+    int16_t y = (buf[5] & 0x0F) << 8 | buf[4];
+
+    data->point.x = x;
+    data->point.y = y;
+    last_x = data->point.x;
+    last_y = data->point.y;
+    data->state = LV_INDEV_STATE_PRESSED;
+  }
+  else
+  {
+    data->point.x = last_x;
+    data->point.y = last_y;
+    data->state = LV_INDEV_STATE_RELEASED;
+  }
+}
 #else
 // GT911
 static void touchpad_read(lv_indev_t* indev, lv_indev_data_t* data)
@@ -51,8 +79,8 @@ static void touchpad_read(lv_indev_t* indev, lv_indev_data_t* data)
 
 void touchpad_init(void)
 {
-//  lv_indev_t* indev = lv_indev_create();
-//  lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
-//  lv_indev_set_read_cb(indev, touchpad_read);
+ lv_indev_t* indev = lv_indev_create();
+ lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+ lv_indev_set_read_cb(indev, touchpad_read);
 }
 
